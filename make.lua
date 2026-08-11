7f7e3f7ac41d023a0b6ce1f3623bb48b41907d38
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-10 20:04:44 -0400

    bump version to 2.0.0-1

--- make.lua
+++ make.lua
@@ -1,16 +1,16 @@
 local env = {
 
   name = "santoku-test-runner",
-  version = "1.0.0-1",
+  version = "2.0.0-1",
   variable_prefix = "TK_TEST_RUNNER",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua == 5.1",
-    "santoku >= 1.0.0, < 2.0.0",
-    "santoku-system >= 1.0.0, < 2.0.0",
-    "santoku-fs >= 1.0.0, < 2.0.0"
+    "santoku >= 2.0.0, < 3.0.0",
+    "santoku-system >= 2.0.0, < 3.0.0",
+    "santoku-fs >= 2.0.0, < 3.0.0"
   },
 
   test = {}
